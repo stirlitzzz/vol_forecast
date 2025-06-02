56de5999748ad12c84f04b1c4c4caadeeff55c55
stirlitzzz <ymulman@yahoo.com>
2025-06-01 21:35:55 -0400

    added forecast scaffodig files

--- src/features.py
+++ src/features.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 def build_har_features(vol_matrix: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-    har_1d = vol_matrix.shift(1)
+    har_1d = vol_matrix.shift(0)
     har_1w = har_1d.rolling(5, min_periods=5).mean()
     har_1m = har_1d.rolling(21, min_periods=21).mean()
     return har_1d, har_1w, har_1m
--- src/forecast.py
+++ src/forecast.py
@@ -4,7 +4,7 @@ import pandas as pd
 def create_forecast(har_1d, har_1w, har_1m, har_betas, av_period=5):
     averaged_betas = har_betas[["beta_0", "beta_1d", "beta_1w", "beta_1m"]]\
                         .rolling(window=av_period, min_periods=av_period)\
-                        .mean().shift(1)
+                        .mean().shift(0)
 
     forecast = (
         averaged_betas["beta_0"].values[:, None]
